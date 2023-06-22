4516c80a2a5d90a27f8378acb26406c3be0cf401
whoisjose569 <jose.vitor.araujo569@hotmail.com>
2023-06-22 09:51:39 -0300

    Configurando arquivos para teste

--- ProjetoFusion/venv/Scripts/activate.bat
+++ ProjetoFusion/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\José Vitor\Desktop\Django\Fusion\venv
+set VIRTUAL_ENV=C:\Users\José Vitor\Desktop\CursoDjango\ProjetoFusion\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
